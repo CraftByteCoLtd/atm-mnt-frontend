963fba55190906819d380cdec2e6b9aebe51a632
Kamon <admin@KamonsMacBook.local>
2017-12-04 19:21:25 +0700

    add logo and detail to display on the homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {JwtHelperService } from '@auth0/angular-jwt';
+import { Component, OnInit , OnDestroy} from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { CompanyProfile } from '../_models/company-profile.model';
+import { AlertService } from '../_services/alert.service';
+import { CompanyProfileService } from '../_services/company-profile.service';
+import { Subscription } from 'rxjs/Subscription';
+
 
 @Component({
   selector: 'app-home',
@@ -7,19 +12,39 @@ import {JwtHelperService } from '@auth0/angular-jwt';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  decodedToken:any;
-  fullName:string;
-  isLoaded:boolean;
-  constructor( private jwtHelperService:JwtHelperService) { }
+  decodedToken: any;
+  fullName: string;
+  isLoaded: boolean;
+  companyProfile: CompanyProfile;
+  profileSubscription: Subscription;
 
-  ngOnInit() {}
-  ngDoCheck(){
-     if(this.jwtHelperService.tokenGetter()){
+
+  constructor(private jwtHelperService: JwtHelperService, private companyProfileService: CompanyProfileService) { }
+
+  ngOnInit() {
+      this.getCompanyProfile(); }
+
+  ngDoCheck() {
+    if (this.jwtHelperService.tokenGetter()) {
       this.decodedToken = this.jwtHelperService.decodeToken();
       this.fullName = this.decodedToken.fullName;
       this.isLoaded = true;
-      
+
     }
   }
+
+  getCompanyProfile() {
+    this.profileSubscription = this.companyProfileService.getCompanyProfiles()
+      .subscribe(response => {
+        this.companyProfile = response[0];
+        console.log(this.companyProfile);
+      },
+      error => console.log(error));
+  }
+
+  ngOnDestroy() {
+    this.profileSubscription.unsubscribe();
+  }
+
 
 }
